f386e67d7cdf8c47a515dcb9c618eadcdfc50ff8
harisdev1 <harisbashirdev@gmail.com>
2025-12-10 14:43:43 +0500

    fix: update API base URL for staging environment

--- lib/src/core/network/config/api_base.dart
+++ lib/src/core/network/config/api_base.dart
@@ -1,6 +1,6 @@
 abstract class ApiBase {
-  // static String baseUrl = "https://lolly-brankiest-wilmer.ngrok-free.dev/";
-  static String baseUrl = "http://163.61.91.146:31146/";
+  static String baseUrl = "http://dinea-staging.qbscocloud.net:30216/";
+  // static String baseUrl = "http://163.61.91.146:31146/";
   static String lockKeysBaseUrl =
       "https://prodgateway.lockkeyz.com/TOTP/ITOTPFeature/";
 }
